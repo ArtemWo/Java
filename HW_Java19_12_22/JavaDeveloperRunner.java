3b8e43fcccf79e43b6c303e95d0a7713ff197d42
ArtemWo <111278541+ArtemWo@users.noreply.github.com>
2022-12-21 15:23:21 +0100

    dz19/12/22

---
+++ HW_Java19_12_22/JavaDeveloperRunner.java
@@ -0,0 +1,20 @@
+package HW_Java.HW_Java19_12_22;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JavaDeveloperRunner {
+    public static void main(String[] args) {
+        String[] skills = {"Java", "Srping", "Hibernate", "Maven", "PostgreSOL"};
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.addAll(List.of(skills));
+
+        JavaDeveloper javaDeveloper = new JavaDeveloper("Eugene", skills);
+        Iterator iterator = javaDeveloper.getIterator(skills);
+        System.out.println("Developer: " + javaDeveloper.getName());
+        System.out.println("Skills:  ");
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next() + "  ");
+        }
+    }
+}
