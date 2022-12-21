e562517e1ba281716c2e1a24b132dfb6398c32f5
ArtemWo <you@example.com>
2022-12-21 13:57:22 +0100

    dz 19/12/22

---
+++ HW_Java19_12_22/Collection.java
@@ -0,0 +1,8 @@
+package HW_Java.HW_Java19_12_22;
+
+public interface Collection {
+    Iterator getIterator(String[] skills);
+
+
+}
+
---
+++ HW_Java19_12_22/Iterator.java
@@ -0,0 +1,7 @@
+package HW_Java.HW_Java19_12_22;
+
+public interface Iterator {
+    boolean hasNext();
+
+     Object next();
+}
