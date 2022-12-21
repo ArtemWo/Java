09906fcdaacebfa3ab880c78293f14223e110a52
ArtemWo <111278541+ArtemWo@users.noreply.github.com>
2022-12-21 15:18:47 +0100

    dz 19/12/22

---
+++ HW_Java19_12_22/Iterator.java
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+package HW_Java.HW_Java19_12_22;
+
+public interface Iterator {
+    boolean hasNext();
+
+     Object next();
+}
+=======
+package HW_Java.HW_Java19_12_22;
+
+public interface Iterator {
+    boolean hasNext();
+
+     Object next();
+}
+>>>>>>> 1fc8157b68a1f22bbe85ca845a21bba226f8d11c
