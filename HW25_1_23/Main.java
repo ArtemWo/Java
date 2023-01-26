5c83640fda9dac586fd407c9de5215cd1911b5c7
ArtemWo <111278541+ArtemWo@users.noreply.github.com>
2023-01-26 15:50:24 +0100

    dz   25_1_23

---
+++ HW25_1_23/Main.java
@@ -0,0 +1,55 @@
+package HW_Java.HW25_1_23;
+
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+/*      todo  1. Напишите код, который создает Stream из массива чисел,
+          фильтрует его по четным числам и выводит результат в консоль.*/
+        int[] num = {22, 33, 3, 43, 8, 99};
+        java.util.Arrays.stream(num).filter(x -> x % 2 == 0).forEach(System.out::println);
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++=");
+
+ /*todo   2. Напишите код, который создает Stream из списка строк,
+            отсортировывает его по длине строк и выводит результат в консоль.*/
+        List<String> list = Arrays.asList("1234", "12345", "123", "12", "123456");
+        list.stream().sorted((x1, x2) -> x1.length() - x2.length()).forEach(System.out::println);
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++=");
+
+/* todo  3. Напишите код, который создает Stream из диапазона чисел,
+           отображает каждое число к его квадрату и выводит результат в консоль.*/
+        IntStream.range(2, 7).map(x -> x * x).forEach(System.out::println);
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++=");
+
+/*todo  4. Напишите код, который создает Stream из списка объектов,
+          объединяет их в одну строку и выводит результат на консоль.*/
+        List<Object> list1 = Arrays.asList("124", 2344, "4321", 45, "wwww");
+        Object qqq = list1.stream().map(Object::toString).collect(Collectors.joining());
+        System.out.println(qqq);
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++=");
+
+/*todo  5. Напишите код, который создает Stream из списка чисел,
+           используя метод reduce () находит наибольшее число в списке.*/
+        List<Integer> list2 = Arrays.asList(2, 23, 45, 22, 1);
+        Optional<Integer> tt = list2.stream().reduce(Integer::max);
+        System.out.println(tt);
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++=");
+
+/* todo  6. Напишите код, который создает Stream из Map, используя метод filter () и метод forEach() выводит
+           ключ - значение только тех элементов, значение которых больше заданного.*/
+        Map<Integer, String> map = new HashMap<>();
+        map.put(23, "aaa");
+        map.put(27, "rr");
+        map.put(4, "uuu");
+        map.put(32, "mmm");
+        map.put(55, "ppp");
+        Stream<Map.Entry<Integer, String>> mapStream = map.entrySet().stream();
+        mapStream.filter(x -> x.getKey() > 20)
+                .forEach(x -> System.out.println(x.getKey() + "     " + x.getValue()));
+
+    }
+}
